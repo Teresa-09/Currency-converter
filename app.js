75b7f01a8927c497bee8cf65db167dd9a34d244b
Teresa-09 <67002949+Teresa-09@users.noreply.github.com>
2021-03-11 13:11:25 +0530

    Create app.js
    We should import converter component in app.js and export converter component.

---
+++ app.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import "./App.css";
+import Converter from "./component/converter";
+class App extends Component {
+  render() {
+    return <Converter />;
+  }
+}
+export default App;
